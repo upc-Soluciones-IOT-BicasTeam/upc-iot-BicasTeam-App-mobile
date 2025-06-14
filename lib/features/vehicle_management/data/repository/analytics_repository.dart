7d1fc62d4d2696a73f485c599263d10e8eaa6d8a
Mauricio Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2025-06-13 23:29:47 -0500

    feat: add analytics feature with driver reports and shipment statistics

---
+++ lib/features/vehicle_management/data/repository/analytics_repository.dart
@@ -0,0 +1,27 @@
+import 'package:movigestion_mobile/features/vehicle_management/data/remote/analytics_service.dart';
+import 'package:movigestion_mobile/features/vehicle_management/data/remote/analytics_model.dart';
+import 'package:movigestion_mobile/features/vehicle_management/data/remote/report_model.dart';
+import 'package:movigestion_mobile/features/vehicle_management/data/remote/shipment_model.dart';
+import 'package:movigestion_mobile/features/vehicle_management/data/remote/vehicle_model.dart';
+
+class AnalyticsRepository {
+  final AnalyticsService analyticsService;
+
+  AnalyticsRepository({required this.analyticsService});
+
+  Future<List<DriverAnalyticsModel>> getDriversAnalytics() async {
+    return await analyticsService.getDriversAnalytics();
+  }
+
+  Future<List<ReportModel>> getReportsByDriverName(String driverName) async {
+    return await analyticsService.getReportsByDriverName(driverName);
+  }
+
+  Future<List<ShipmentModel>> getShipmentsByDriverName(String driverName) async {
+    return await analyticsService.getShipmentsByDriverName(driverName);
+  }
+
+  Future<List<VehicleModel>> getVehiclesByDriverName(String driverName) async {
+    return await analyticsService.getVehiclesByDriverName(driverName);
+  }
+}
